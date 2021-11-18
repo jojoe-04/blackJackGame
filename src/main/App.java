05f27b807d2c67ff50c4d8e5bf9b8189698db17b
jojoe-04 <jojoeainoo@turntabl.io>
2021-11-18 12:19:02 +0000

    Card Class, Suit Enum, Value Enum

--- src/main/App.java
+++ src/main/App.java
@@ -1,2 +1,12 @@
+import card.CardValue;
+import card.Suit;
+
 public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Black Jack Game");
+
+        Card card = new Card(Suit.Heart, CardValue.EIGHT);
+
+    }
 }
