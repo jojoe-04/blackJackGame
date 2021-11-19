3a021563f25b4af9ad7935d552004dde4620fe28
jojoe-04 <jojoeainoo@turntabl.io>
2021-11-19 09:23:30 +0000

    refactor player class & document player class

--- src/main/game/Player.java
+++ src/main/game/Player.java
@@ -5,6 +5,9 @@ import card.Card;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * The type Player.
+ */
 public class Player {
     private final String name;
     private final String playerId;
@@ -12,6 +15,11 @@ public class Player {
     private final List<Card> hand;
     private String turn;
 
+    /**
+     * Instantiates a new Player.
+     * @param playerId the player id
+     * @param name     the player name
+     */
     public Player(String playerId, String name) {
         this.playerId = playerId;
         this.name = name;
@@ -20,48 +28,65 @@ public class Player {
         this.turn = "";
     }
 
+    /**
+     * Add card to player hand list
+     * @param card the card
+     */
     public void addToHand(Card card) {
         this.hand.add(card);
     }
 
-
-    public int getPoints() {
-       this.caculatePoints();
-       return points;
-    }
-
+    /**
+     * Gets hand list of player cards
+     * @return the hand
+     */
     public List<Card> getHand() {
         return hand;
     }
 
-    //TODO refactor
-    public void calculateTurns() {
-        if(this.getPoints() < 17)  turn = "hit";
-
-        if(this.getPoints() >= 17)  turn = "stick";
-
-        if(this.getPoints() > 21) turn = "bust";
-
+    /**
+     * Uses Streams to Calculate Points of Cards in Player's hands
+     */
+    private void calculatePoints() {
+        this.points = 0;
+        this.points = hand.stream().map(card -> card.getCardValue().getValue()).reduce(0,Integer::sum);
     }
 
-    //TODO refactor
+    /**
+     * Calculate points & Gets points.
+     * @return the points
+     */
+    public int getPoints() {
+        this.calculatePoints();
+        return points;
+    }
+
+    /**
+     * Calculate turns by assigning turn value to player based on points
+     */
+    public void calculateTurns() {
+        if(this.getPoints() < 17) turn = "hit";
+        if(this.getPoints() >= 17) turn = "stick";
+        if(this.getPoints() > 21) turn = "bust";
+    }
+
+    /**
+     * Calculates turns and return player's turn
+     * @return turn
+     */
     public String getTurn() {
         this.calculateTurns();
         return turn;
     }
 
+    /**
+     * Gets name of player
+     * @return the name
+     */
     public String getName() {
         return name;
     }
 
-    //TODO refactor to use streams
-    public void caculatePoints(){
-        this.points = 0;
-        for(Card c : this.hand){
-            this.points += c.getCardValue().getValue();
-        }
-
-    }
 
     @Override
     public String toString() {
@@ -73,6 +98,4 @@ public class Player {
                 ", turn=" + turn +
                 '}';
     }
-
-
 }
